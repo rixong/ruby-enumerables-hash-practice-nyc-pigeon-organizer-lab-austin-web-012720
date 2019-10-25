67b8cbeb4499f04bc93b4bf85ee117b21421c656
Rick Glascock <rixong@gmail.com>
2019-10-25 04:30:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    pigeon_list = {}
+    # iterate over gender hash to collect names, and create empty hashes (color, lives keys)
+    data[:gender].each_pair do |(key, value)|
+       data[:gender][key].map {|name| pigeon_list[name] = {:color => [], :gender => [], :lives => []}}
+    end
+    
+    # iterate through other attributes to fill in data
+
+    data.each_pair do |(key1, value1)|
+      data[key1].each_pair do |(key,value)|
+        data[key1][key].map {|name| pigeon_list[name][key1].push(key.to_s)}
+      end
+    end
+    return pigeon_list 
 end
