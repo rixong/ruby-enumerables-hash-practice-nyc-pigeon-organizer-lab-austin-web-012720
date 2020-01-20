641e40e22762fb5824ec83a2c50364d825e9eedb
Rick Glascock <rixong@gmail.com>
2020-01-20 15:46:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,12 +5,12 @@ def nyc_pigeon_organizer(data)
   }
   # color 
   
-  data.each_key { | key | 
-    data[key].each_pair { | k,v| 
-    v.each { |name| pigeon_list[name][key].push(k.to_s)
-      }
-    }
-  }
+  # data.each_key { | key | 
+  #   data[key].each_pair { | k,v| 
+  #   v.each { |name| pigeon_list[name][key].push(k.to_s)
+  #     }
+  #   }
+  # }
 
   
   return pigeon_list
